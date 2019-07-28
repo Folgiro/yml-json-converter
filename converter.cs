e631e15357edf4fa46d02e21b2fe4304e19b6964
Christian Schultze <christian.schultze98@gmx.de>
2019-07-28 22:54:00 +0200

    initial commit

---
+++ converter.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.IO;
+using YamlDotNet.Serialization;
+
+class Converter
+{
+
+    static void Main(string[] args)
+    {
+        convert("config.yml");
+
+        //write in to json file File.WriteAllLines(@"C:\Users\Public\TestFolder\WriteLines.txt", lines);
+        System.Environment.Exit(0);
+    }
+
+    static string convert(string filename)
+    {
+        
+        var serializer = new Serializer();
+        var deserializer = new Deserializer();
+        var writer = new StringWriter();
+
+        var r = new StreamReader(filename);
+        var yamlObject = deserializer.Deserialize(r);
+
+        serializer.Serialize(writer, yamlObject);
+        string json = writer.ToString();
+
+
+    }
+
+
+    static string loadAdditionalFiles(string[] files)
+    {
+
+    }
+}
